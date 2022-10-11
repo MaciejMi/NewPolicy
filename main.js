49de3636f70b26fb75a8276885bce84841e26c88
Greemi <geniuszek22@gmail.com>
2022-10-11 19:31:23 +0200

    created navbar

---
+++ main.js
@@ -0,0 +1,21 @@
+const nav = document.querySelector('nav ul');
+const openBtn = document.querySelector('#open-menu-btn');
+const closeBtn = document.querySelector('#close-menu-btn');
+
+openBtn.addEventListener('click', () => {
+  nav.classList.add('active');
+  openBtn.classList.remove('active');
+  closeBtn.classList.add('active');
+});
+
+closeBtn.addEventListener('click', () => {
+  nav.classList.remove('active');
+  openBtn.classList.add('active');
+  closeBtn.classList.remove('active');
+});
+
+window.addEventListener('scroll', () => {
+  nav.classList.remove('active');
+  openBtn.classList.add('active');
+  closeBtn.classList.remove('active');
+});
