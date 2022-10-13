c1a4477068fb10093793362d2cecac4601c654e9
Greemi <geniuszek22@gmail.com>
2022-10-13 14:33:23 +0200

    added hidden news

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const nav = document.querySelector('nav ul');
 const openBtn = document.querySelector('#open-menu-btn');
 const closeBtn = document.querySelector('#close-menu-btn');
+const showMoreBtn = document.querySelector('#show-more-btn');
+const hiddenNews = document.querySelector('.hidden');
 
 openBtn.addEventListener('click', () => {
   nav.classList.add('active');
@@ -18,4 +20,14 @@ window.addEventListener('scroll', () => {
   nav.classList.remove('active');
   openBtn.classList.add('active');
   closeBtn.classList.remove('active');
+});
+
+showMoreBtn.addEventListener('click', () => {
+  if(hiddenNews.style.display === 'none'){
+    hiddenNews.style.display = 'block';
+    showMoreBtn.textContent = 'Ukryj';
+  } else {
+    hiddenNews.style.display = 'none';
+    showMoreBtn.textContent = 'Wyświetl więcej';
+  }
 });
